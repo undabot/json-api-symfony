70c970810554cf963a6bda9408f9a98795668df8
Tomo Šala <tomo.sala@undabot.com>
2019-02-04 16:37:20 +0100

    Add support for collection of included resources to ResourceJsonApiResponse
    JsonApiResponse didn't handle included resources.
    EncoderListener now retrieves a collection of included resources from
    JsonApiResponse object and explicitly sets it as `Document` property.

--- src/Response/ResourceJsonApiResponse.php
+++ src/Response/ResourceJsonApiResponse.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Undabot\SymfonyJsonApi\Response;
 
 use Symfony\Component\HttpFoundation\Response;
+use Undabot\JsonApi\Model\Resource\ResourceCollection;
 use Undabot\JsonApi\Model\Resource\ResourceInterface;
 
 class ResourceJsonApiResponse extends Response implements JsonApiResponseInterface
@@ -12,14 +13,24 @@ class ResourceJsonApiResponse extends Response implements JsonApiResponseInterfa
     /** @var ResourceInterface */
     private $jsonApiResource;
 
-    public function __construct(ResourceInterface $resource, array $headers = [])
+    /** @var ResourceCollection|null */
+    private $includedResources;
+
+    public function __construct(ResourceInterface $resource, ?ResourceCollection $includedResources, array $headers =
+    [])
     {
         parent::__construct(null, Response::HTTP_OK, $headers);
         $this->jsonApiResource = $resource;
+        $this->includedResources = $includedResources;
     }
 
     public function getJsonApiResource(): ResourceInterface
     {
         return $this->jsonApiResource;
+    }
+
+    public function getIncludedResources(): ?ResourceCollection
+    {
+        return $this->includedResources;
     }
 }

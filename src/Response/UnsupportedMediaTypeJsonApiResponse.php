221babc95cfbfcd80715424a16889e8d83685aab
Tomo Šala <tomo.sala@undabot.com>
2019-02-06 15:48:11 +0000

    Register exception listeners

---
+++ src/Response/UnsupportedMediaTypeJsonApiResponse.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Undabot\SymfonyJsonApi\Response;
+
+use Symfony\Component\HttpFoundation\Response;
+use Undabot\SymfonyJsonApi\Request\Exception\InvalidRequestContentTypeHeaderException;
+
+class UnsupportedMediaTypeJsonApiResponse extends AbstractErrorJsonApiResponse
+{
+    public function __construct(InvalidRequestContentTypeHeaderException $exception, array $headers = [])
+    {
+        parent::__construct(null, Response::HTTP_UNSUPPORTED_MEDIA_TYPE, $headers);
+        $this->message = $exception->getMessage();
+    }
+}

2205dc91657ae130b2cb56fc8b0ce5e4cf7e8067
Tomo Šala <tomo.sala@undabot.com>
2019-02-08 10:07:04 +0100

    Code style cleanup in JsonApiRequestFactory
    Add return type hints to methods, rename variable name representing
    ResourceEncoder to match the new type name introduced in json-api-core
    (all encoders/decoders were moved to `Encoding` NS and renamed to be
    more indicative of their intent).

--- src/Request/Factory/JsonApiRequestFactory.php
+++ src/Request/Factory/JsonApiRequestFactory.php
@@ -8,6 +8,7 @@ use InvalidArgumentException;
 use Symfony\Component\HttpFoundation\Request;
 use Undabot\JsonApi\Encoding\Exception\PhpArrayEncodingException;
 use Undabot\JsonApi\Encoding\PhpArrayToResourceEncoderInterface;
+use Undabot\JsonApi\Model\Request\CreateResourceRequestInterface;
 use Undabot\JsonApi\Model\Resource\ResourceInterface;
 use Undabot\JsonApi\Util\Assert\Assert;
 use Undabot\SymfonyJsonApi\Request\CreateResourceRequest;
@@ -84,9 +85,7 @@ class JsonApiRequestFactory
         }
 
         $requestPrimaryData['id'] = $id;
-        $resource = $this->phpArrayToResourceEncoder->decode($requestPrimaryData);
-
-        return $resource;
+        return $this->phpArrayToResourceEncoder->decode($requestPrimaryData);
     }
 
     /**
@@ -94,9 +93,7 @@ class JsonApiRequestFactory
      */
     private function getSingleResourceObjectFromRequest(array $requestPrimaryData): ResourceInterface
     {
-        $resource = $this->phpArrayToResourceEncoder->decode($requestPrimaryData);
-
-        return $resource;
+        return $this->phpArrayToResourceEncoder->decode($requestPrimaryData);
     }
 
     /**
@@ -107,8 +104,9 @@ class JsonApiRequestFactory
      * @throws InvalidRequestContentTypeHeaderException
      * @throws InvalidRequestDataException
      * @throws PhpArrayEncodingException
+     * @throws UnsupportedQueryStringParameterGivenException
      */
-    public function makeCreateResourceRequestWithServerGeneratedId(Request $request, string $id)
+    public function makeCreateResourceRequestWithServerGeneratedId(Request $request, string $id): CreateResourceRequestInterface
     {
         return $this->makeCreateResourceRequest($request, false, $id);
     }
@@ -178,12 +176,12 @@ class JsonApiRequestFactory
     /**
      * @throws InvalidRequestAcceptHeaderException
      * @throws InvalidRequestContentTypeHeaderException
-     * @throws UnsupportedQueryStringParameterGivenException
-     * @throws UnsupportedIncludeValuesGivenException
      * @throws UnsupportedFilterAttributeGivenException
+     * @throws UnsupportedIncludeValuesGivenException
+     * @throws UnsupportedPaginationRequestedException
+     * @throws UnsupportedQueryStringParameterGivenException
      * @throws UnsupportedSortRequestedException
      * @throws UnsupportedSparseFieldsetRequestedException
-     * @throws UnsupportedPaginationRequestedException
      */
     public function makeGetResourceCollectionRequest(
         Request $request,

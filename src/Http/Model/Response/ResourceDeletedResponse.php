7e64260fd3af3a5ffd73236977e9209c0b17a942
marko <marko.samec@undabot.com>
2019-11-13 19:07:38 +0100

    Remove unused includes

--- src/Http/Model/Response/ResourceDeletedResponse.php
+++ src/Http/Model/Response/ResourceDeletedResponse.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace Undabot\SymfonyJsonApi\Http\Model\Response;
 
-use Undabot\JsonApi\Model\Link\LinkCollectionInterface;
-use Undabot\JsonApi\Model\Meta\MetaInterface;
-use Undabot\JsonApi\Model\Resource\ResourceCollectionInterface;
-use Undabot\JsonApi\Model\Resource\ResourceInterface;
-
 final class ResourceDeletedResponse
 {
 

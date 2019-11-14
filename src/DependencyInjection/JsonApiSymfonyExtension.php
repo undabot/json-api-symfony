700d1fe8e005f2e121e178e42fac10b8434b5bef
marko <marko.samec@undabot.com>
2019-11-14 09:40:37 +0100

    Use Definition to register ExceptionListener

--- src/DependencyInjection/JsonApiSymfonyExtension.php
+++ src/DependencyInjection/JsonApiSymfonyExtension.php
@@ -7,8 +7,11 @@ namespace Undabot\SymfonyJsonApi\DependencyInjection;
 use Exception;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Reference;
+use Undabot\JsonApi\Encoding\DocumentToPhpArrayEncoderInterface;
 use Undabot\SymfonyJsonApi\Exception\EventSubscriber\ExceptionListener;
 
 class JsonApiSymfonyExtension extends Extension
@@ -26,11 +29,17 @@ class JsonApiSymfonyExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $definition = $container->getDefinition(ExceptionListener::class);
-        $tag = $definition->getTag('kernel.event_listener');
-        $tag[0]['priority'] = $config['exception_listener_priority'] ?? self::EXCEPTION_LISTENER_PRIORITY;
-        $definition->setTags([
-            'kernel.event_listener' => $tag,
+        $definition = new Definition(ExceptionListener::class, [
+            new Reference(DocumentToPhpArrayEncoderInterface::class)
         ]);
+        $definition->setTags([
+            'kernel.event_listener' => [
+                [
+                    'event' => 'kernel.exception',
+                    'priority' => $config['exception_listener_priority'] ?? self::EXCEPTION_LISTENER_PRIORITY
+                ]
+            ]
+        ]);
+        $container->setDefinition('json_api_symfony.exception_listener', $definition);
     }
 }

0a5d75be34545e0b41a9b78367dd1c6a5c10a052
Ivan Penovic <ivan.penovic222@gmail.com>
2020-01-08 22:49:30 +0100

    Add get resource request test

---
+++ tests/Unit/Http/Service/Factory/RequestFactoryTest.php
@@ -0,0 +1,85 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Undabot\JsonApi\Tests\Unit\Http\Service\Factory;
+
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+use Undabot\JsonApi\Definition\Encoding\PhpArrayToResourceEncoderInterface;
+use Undabot\SymfonyJsonApi\Http\Model\Request\GetResourceRequest;
+use Undabot\SymfonyJsonApi\Http\Service\Factory\RequestFactory;
+use Undabot\SymfonyJsonApi\Http\Service\Validation\RequestValidator;
+
+final class RequestFactoryTest extends TestCase
+{
+    /** @var MockObject */
+    private $resourceEncoder;
+
+    /** @var MockObject */
+    private $requestValidator;
+
+    /** @var RequestFactory */
+    private $requestFactory;
+
+    protected function setUp(): void
+    {
+        $this->resourceEncoder = $this->createMock(PhpArrayToResourceEncoderInterface::class);
+        $this->requestValidator = $this->createMock(RequestValidator::class);
+        $this->requestFactory = new RequestFactory($this->resourceEncoder, $this->requestValidator);
+    }
+
+    /**
+     * @dataProvider requestParamsProvider
+     */
+    public function testGetResourceRequestWillReturnValidGetResourceRequestGivenValidRequest(array $queryParams, ?array $include, ?array $fields): void
+    {
+        $id = '123';
+
+        $resourceRequest = new GetResourceRequest($id, $include, $fields);
+
+        $request = $this->createMock(Request::class);
+        $query = new ParameterBag($queryParams);
+        $request->query = $query;
+        $this->requestValidator->expects($this->once())->method('assertValidRequest');
+
+        $getResourceRequest = $this->requestFactory->getResourceRequest($request, $id);
+
+        $this->assertEquals($resourceRequest, $getResourceRequest);
+    }
+
+    public function requestParamsProvider(): \Generator
+    {
+        yield 'No include and no fields in request get params' => [
+            [],
+            null,
+            null
+        ];
+
+        yield 'No include and fields present in request get params' => [
+            ['fields' => ['foo', 'bar']],
+            null,
+            ['foo', 'bar']
+        ];
+
+        yield 'Include string provided with multiple values and no fields in request get params' => [
+            ['include' => 'foo,bar,baz'],
+            ['foo', 'bar', 'baz'],
+            null
+        ];
+
+        yield 'Include string provided with single value and no fields in request get params' => [
+            ['include' => 'foo'],
+            ['foo'],
+            null
+        ];
+
+        yield 'Both include and fields provided in request get params' => [
+            ['include' => 'foo,bar,baz', 'fields' => ['foo', 'bar']],
+            ['foo', 'bar', 'baz'],
+            ['foo', 'bar']
+        ];
+    }
+}

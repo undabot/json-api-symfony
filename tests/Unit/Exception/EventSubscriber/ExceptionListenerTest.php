b9f3d2a89903799977352e269213950b24300764
Ivan Penovic <ivan.penovic222@gmail.com>
2020-01-09 13:47:11 +0100

    Test Resource collection response creation

--- tests/Unit/Exception/EventSubscriber/ExceptionListenerTest.php
+++ tests/Unit/Exception/EventSubscriber/ExceptionListenerTest.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Undabot\JsonApi\Tests\Unit\Http\Service\Factory;
+namespace Undabot\JsonApi\Tests\Unit\Exception\EventSubscriber;
 
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;

4a4ea80f38424f52acaf69dd5249f37a54166ea0
marko <marko.samec@undabot.com>
2020-05-27 22:07:16 +0200

    feat(open api support): generate yaml

--- src/Bridge/OpenApi/ApiTransformer.php
+++ src/Bridge/OpenApi/ApiTransformer.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace Undabot\SymfonyJsonApi\Bridge\OpenApi;
 
+use Symfony\Component\Yaml\Yaml;
 use Undabot\SymfonyJsonApi\Bridge\OpenApi\Model\Api;
 
 final class ApiTransformer
 {
+    public function toYaml(Api $api): string
+    {
+        $array = $api->toOpenApi();
+
+        return Yaml::dump($array, 12);
+    }
+
     public function toJson(Api $api): string
     {
         return json_encode(

97352843b2f69a6a2887cf1b8fd64c85b48b51f9
Ivan Penovic <ivan.penovic222@gmail.com>
2022-12-08 16:12:19 +0100

    refactor(link): pagination creation

---
+++ src/Model/Link/ResponsePaginationLink.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Undabot\SymfonyJsonApi\Model\Link;
+
+/** @psalm-immutable */
+final class ResponsePaginationLink
+{
+    public function __construct(
+        public string $paginationPageKey,
+        public int $nextSet,
+        public int $previousSet,
+        public int $firstPageKey,
+        public ?int $lastPageKey
+    ) {
+    }
+}

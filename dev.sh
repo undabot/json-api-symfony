c362edb5b09319807c0990147a4e9986536b7171
Ivan Penovic <ivan.penovic222@gmail.com>
2023-09-01 10:01:51 +0200

    feat(build): test supported versions

--- dev.sh
+++ dev.sh
@@ -4,6 +4,7 @@ DOCKERFILE=docker/php82.Dockerfile
 PROJECTNAME=json_api_symfony_dev
 CONTAINERNAME=json_api_symfony
 EXEC="docker exec -it $CONTAINERNAME /bin/bash"
+PHP_VERSIONS_TO_TEST=( "80" "81" "82" "83" )
 
 build_base_docker_image () {
   docker build -f $DOCKERFILE -t $PROJECTNAME .
@@ -40,6 +41,20 @@ run_install () {
   $EXEC -c "composer install --optimize-autoloader"
 }
 
+run_test_php_versions () {
+  for version in "${PHP_VERSIONS_TO_TEST[@]}"
+  do
+    docker stop docker/php$version.Dockerfile > /dev/null 2>&1
+    docker container rm $CONTAINERNAME
+    docker build -f docker/php$version.Dockerfile -t $PROJECTNAME .
+    docker run --name $CONTAINERNAME --mount type=bind,source="$(pwd)",target=/opt/app -d $PROJECTNAME
+    $EXEC -c "cd /opt/app && composer validate --strict && composer outdated --strict && composer install --optimize-autoloader"
+    $EXEC -c "composer install --optimize-autoloader"
+    docker stop docker/php$version.Dockerfile > /dev/null 2>&1
+    docker container rm $CONTAINERNAME
+  done
+}
+
 case $1 in
       build) build_base_docker_image
              stop_container
@@ -53,4 +68,5 @@ case $1 in
       test) run_tests ;;
       install) run_install ;;
       qc) run_qc ;;
+      test_php_versions) run_test_php_versions ;;
 esac

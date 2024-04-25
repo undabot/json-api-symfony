c8b0d1d3c4f793d1d8e98d72b2329b6a3e62db21
Ivan Penovic <ivan.penovic222@gmail.com>
2024-04-25 10:25:36 +0200

    fix: add bundle loader

--- src/JsonApiSymfonyBundle.php
+++ src/JsonApiSymfonyBundle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Undabot\SymfonyJsonApi;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Undabot\SymfonyJsonApi\DependencyInjection\Compiler\ApiGeneratorPass;
 
@@ -15,5 +16,10 @@ class JsonApiSymfonyBundle extends AbstractBundle
         parent::build($container);
 
         $container->addCompilerPass(new ApiGeneratorPass());
+    }
+
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('DependencyInjection/config/services.yaml');
     }
 }
